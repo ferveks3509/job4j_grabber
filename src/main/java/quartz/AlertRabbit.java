5fdb0b2c872ab6a9a933389fe349bcf8664b4115
Илья <qq.ferveks@gmail.com>
2021-07-13 21:08:13 +0300

    1. Quartz [#175122]

---
+++ src/main/java/quartz/AlertRabbit.java
@@ -0,0 +1,26 @@
+package quartz;
+
+import org.quartz.*;
+import org.quartz.impl.StdSchedulerFactory;
+
+import static org.quartz.SimpleScheduleBuilder.simpleSchedule;
+
+public class AlertRabbit {
+    public static void main(String[] args) {
+        try {
+            Scheduler scheduler = StdSchedulerFactory.getDefaultScheduler();
+            scheduler.start();
+            JobDetail job = JobBuilder.newJob(Rabbit.class).build();
+            SimpleScheduleBuilder times = simpleSchedule()
+                    .withIntervalInSeconds(10)
+                    .repeatForever();
+            Trigger trigger = TriggerBuilder.newTrigger()
+                    .startNow()
+                    .withSchedule(times)
+                    .build();
+            scheduler.scheduleJob(job, trigger);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
