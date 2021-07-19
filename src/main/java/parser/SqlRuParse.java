a9c99b2573e35d94eb3334fb41c30f8fc922c132
Илья <qq.ferveks@gmail.com>
2021-07-19 22:46:39 +0300

    2.1.1. Парсинг https://www.sql.ru/forum/job-offers/3 [#285210]

--- src/main/java/parser/SqlRuParse.java
+++ src/main/java/parser/SqlRuParse.java
@@ -9,14 +9,16 @@ import org.jsoup.select.Elements;
 public class SqlRuParse {
 
     public static void main(String[] args) throws Exception {
-        Document document = Jsoup.connect("https://www.sql.ru/forum/job-offers").get();
-        Elements row = document.select(".postslisttopic");
-        for (Element el : row) {
-            Element href = el.child(0);
-            Element time = el.parent().child(5);
-            System.out.println(href.attr("href"));
-            System.out.println(href.text());
-            System.out.println(time.text());
+        for (int i = 1; i <= 5; i++) {
+            Document document = Jsoup.connect("https://www.sql.ru/forum/job-offers/" + i).get();
+            Elements row = document.select(".postslisttopic");
+            for (Element el : row) {
+                Element href = el.child(0);
+                Element time = el.parent().child(5);
+                System.out.println(href.attr("href"));
+                System.out.println(href.text());
+                System.out.println(time.text());
+            }
         }
     }
 }
