22edbc8f6d2b8cc9c87bdbd9392524b8127267a4
Илья <qq.ferveks@gmail.com>
2021-07-24 19:05:26 +0300

    2.1. Преобразование даты [#289476 #179488]

--- src/main/java/parser/SqlRuDateTimeParser.java
+++ src/main/java/parser/SqlRuDateTimeParser.java
@@ -6,6 +6,20 @@ import java.util.Map;
 
 public class SqlRuDateTimeParser implements DateTimeParser {
     private static final Map<String, Integer> month = new HashMap<>();
+    {
+        month.put("янв", 1);
+        month.put("фев", 2);
+        month.put("мар", 3);
+        month.put("апр", 4);
+        month.put("май", 5);
+        month.put("июн", 6);
+        month.put("июл", 7);
+        month.put("авг", 8);
+        month.put("сен", 9);
+        month.put("окт", 10);
+        month.put("ноя", 11);
+        month.put("дек", 12);
+    }
 
     @Override
     public LocalDateTime parse(String s) {
@@ -14,7 +28,6 @@ public class SqlRuDateTimeParser implements DateTimeParser {
         } else if (s.contains("вчера")) {
             return shortDate(s).minusDays(1);
         }
-        initMonth();
         String[] arrData = s.split(" ");
         int years = Integer.parseInt(arrData[2].substring(0,1));
         int month = this.month.get(arrData[1]);
@@ -30,20 +43,5 @@ public class SqlRuDateTimeParser implements DateTimeParser {
         LocalTime localTime = LocalTime.of(hh, mm);
         LocalDate localDate = LocalDate.now();
         return LocalDateTime.of(localDate, localTime);
-    }
-
-    private void initMonth() {
-        month.put("янв", 1);
-        month.put("фев", 2);
-        month.put("мар", 3);
-        month.put("апр", 4);
-        month.put("май", 5);
-        month.put("июн", 6);
-        month.put("июл", 7);
-        month.put("авг", 8);
-        month.put("сен", 9);
-        month.put("окт", 10);
-        month.put("ноя", 11);
-        month.put("дек", 12);
     }
 }
